0c0fbfbe98c119ae08b8ab266e8d1ec173437045
Pavle Sarenac <sarenac.pavle@gmail.com>
2023-08-13 22:43:16 +0200

    Folder restructuring.

---
+++ h/Testing/Testing_OS1/Buffer.hpp
@@ -0,0 +1,29 @@
+#ifndef BUFFER_HPP
+#define BUFFER_HPP
+
+#include "../../Code/SystemCalls/syscall_c.h"
+#include "Printing.hpp"
+
+class Buffer {
+private:
+    int cap;
+    int *buffer;
+    int head, tail;
+
+    sem_t spaceAvailable;
+    sem_t itemAvailable;
+    sem_t mutexHead;
+    sem_t mutexTail;
+
+public:
+    Buffer(int _cap);
+    ~Buffer();
+
+    void put(int val);
+    int get();
+
+    int getCnt();
+
+};
+
+#endif
---
+++ h/Testing/Testing_OS1/Printing.hpp
@@ -0,0 +1,18 @@
+#ifndef PRINTING_HPP
+#define PRINTING_HPP
+
+#include "../../Code/SystemCalls/syscall_c.h"
+
+typedef unsigned long uint64;
+
+extern "C" uint64 copy_and_swap(uint64 &lock, uint64 expected, uint64 desired);
+
+void printString(char const *string);
+
+char* getString(char *buf, int max);
+
+int stringToInt(const char *s);
+
+void printInt(int xx, int base=10, int sgn=0);
+
+#endif
