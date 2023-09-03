32cdd816a3efce7582b402e7c8ce95a3c5b8b0dc
Pavle Sarenac <sarenac.pavle@gmail.com>
2023-09-03 17:19:02 +0200

    BuddyAllocator initialization and allocation done.

--- h/Code/MemoryAllocator/BuddyAllocator.hpp
+++ h/Code/MemoryAllocator/BuddyAllocator.hpp
@@ -11,17 +11,19 @@ public:
     static BuddyAllocator& getInstance();
     BuddyAllocator(const BuddyAllocator&) = delete;
     void operator=(const BuddyAllocator&) = delete;
-    void setup(void* firstAlignedAddress, int totalNumberOfMemoryBlocks);
+    void setup(void* firstAlignedAddress, int totalNumberOfBlocks);
+    void* allocate(int numberOfBytes);
 private:
     int numberOfBlocksOfSameSize[MAX_EXPONENT + 1];
     bool isBlockFree[MAX_EXPONENT + 1][MAX_NUMBER_OF_BLOCKS_OF_SAME_SIZE];
-
     void* firstAlignedAddress;
-    int maxUsedExponent;
-    int maxUsedNumberOfBlocksOfSameSize;
+    int maxUsedExponent, maxUsedNumberOfBlocksOfSameSize;
 
     BuddyAllocator();
-    static int getMaxUsedExponent(int totalNumberOfMemoryBlocks);
+    static int getExponentForNumberOfBlocks(int numberOfBlocks);
+    static int getExponentForNumberOfBytes(int numberOfBytes);
+    int getFreeBlockIndex(int exponent) const;
+    void* getBlockAddress(int exponent, int blockIndex) const;
 };
 
 #endif
--- src/Code/MemoryAllocator/BuddyAllocator.cpp
+++ src/Code/MemoryAllocator/BuddyAllocator.cpp
@@ -1,31 +1,67 @@
 #include "../../../h/Code/MemoryAllocator/BuddyAllocator.hpp"
+#include "../../../h/Code/MemoryAllocator/slab.hpp"
 
-BuddyAllocator::BuddyAllocator() {
-    for (int i = MAX_EXPONENT, numberOfBlocks = 1; i >= 0; i--, numberOfBlocks <<= 1) {
-        numberOfBlocksOfSameSize[i] = numberOfBlocks;
-        for (int j = 0; j < MAX_NUMBER_OF_BLOCKS_OF_SAME_SIZE; j++) {
-            isBlockFree[i][j] = false;
-        }
-    }
-    isBlockFree[MAX_EXPONENT][0] = true;
-}
+BuddyAllocator::BuddyAllocator() {}
 
 BuddyAllocator& BuddyAllocator::getInstance() {
     static BuddyAllocator buddyAllocator;
     return buddyAllocator;
 }
 
-void BuddyAllocator::setup(void* firstAlignedAddress, int totalNumberOfMemoryBlocks) {
+void BuddyAllocator::setup(void* firstAlignedAddress, int totalNumberOfBlocks) {
     this->firstAlignedAddress = firstAlignedAddress;
-    this->maxUsedExponent = getMaxUsedExponent(totalNumberOfMemoryBlocks);
+    this->maxUsedExponent = getExponentForNumberOfBlocks(totalNumberOfBlocks);
     this->maxUsedNumberOfBlocksOfSameSize = 1 << this->maxUsedExponent;
+    if (maxUsedExponent > MAX_EXPONENT) return;
+    for (int i = maxUsedExponent, numberOfBlocks = 1; i >= 0; i--, numberOfBlocks <<= 1) {
+        numberOfBlocksOfSameSize[i] = numberOfBlocks;
+        for (int j = 0; j < maxUsedNumberOfBlocksOfSameSize; j++) {
+            isBlockFree[i][j] = false;
+        }
+    }
+    isBlockFree[this->maxUsedExponent][0] = true;
 }
 
-int BuddyAllocator::getMaxUsedExponent(int totalNumberOfMemoryBlocks) {
-    int maxUsedExponent = 0;
-    for (int i = 0; totalNumberOfMemoryBlocks; i++) {
-        if (i > 0) maxUsedExponent++;
-        totalNumberOfMemoryBlocks >>= 1;
+void* BuddyAllocator::allocate(int numberOfBytes) {
+    int minNeededExponent = getExponentForNumberOfBytes(numberOfBytes);
+    int currentExponent = minNeededExponent;
+    int freeBlockIndex = -1;
+    while (currentExponent <= maxUsedExponent) {
+        freeBlockIndex = getFreeBlockIndex(currentExponent);
+        if (freeBlockIndex >= 0) break;
+        currentExponent++;
     }
-    return maxUsedExponent;
+    if (freeBlockIndex == -1) return nullptr;
+    isBlockFree[currentExponent][freeBlockIndex] = false;
+    while (--currentExponent >= minNeededExponent) {
+        freeBlockIndex <<= 1;
+        isBlockFree[currentExponent][freeBlockIndex + 1] = true;
+    }
+    return getBlockAddress(minNeededExponent, freeBlockIndex);
+}
+
+int BuddyAllocator::getExponentForNumberOfBlocks(int numberOfBlocks) {
+    int exponent = 0;
+    for (int i = 0; numberOfBlocks; i++) {
+        if (i > 0) exponent++;
+        numberOfBlocks >>= 1;
+    }
+    return exponent;
+}
+
+int BuddyAllocator::getExponentForNumberOfBytes(int numberOfBytes) {
+    int numberOfBlocks = numberOfBytes / BLOCK_SIZE + ((numberOfBytes % BLOCK_SIZE != 0) ? 1 : 0);
+    return getExponentForNumberOfBlocks(numberOfBlocks);
+}
+
+int BuddyAllocator::getFreeBlockIndex(int exponent) const {
+    for (int i = 0; i < numberOfBlocksOfSameSize[exponent]; i++)
+        if (isBlockFree[exponent][i]) return i;
+    return -1;
+}
+
+void* BuddyAllocator::getBlockAddress(int exponent, int blockIndex) const {
+     return static_cast<char*>(firstAlignedAddress)
+            + exponent * (maxUsedNumberOfBlocksOfSameSize * BLOCK_SIZE)
+            + blockIndex * BLOCK_SIZE;
 }
